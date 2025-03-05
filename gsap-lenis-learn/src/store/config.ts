bb8b61be2a0dc3efbb72221580b9f3b64e67bd86
Capsion <373704015@qq.com>
2025-03-06 00:08:46 +0800

    feat: fast

---
+++ gsap-lenis-learn/src/store/config.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_SUB_COLOR = ['#FF4058', '#F6B429', '#64D487', '#1D72B8', '#FF7F32', '#8E44AD'];
+export const DEFAULT_MAIN_COLOR = ['#FC1E4F', '#FFF43D', '#9FDA7F', '#4A90E2', '#FF9F00', '#9B4DCA'];
