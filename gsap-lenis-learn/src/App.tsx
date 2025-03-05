b9aedaa968f0778742ce0f5f8b1dd900fe75e8d3
capsion_surfacePro7 <capsion@surfacePro2.com>
2025-03-05 21:41:58 +0800

    chore: add:tailwindCSS

--- gsap-lenis-learn/src/App.tsx
+++ gsap-lenis-learn/src/App.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: capsion_surfacePro7 capsion@surfacePro2.com
+ * @Date: 2025-03-05 20:58:38
+ * @LastEditors: capsion_surfacePro7 capsion@surfacePro2.com
+ * @LastEditTime: 2025-03-05 21:30:21
+ * @FilePath: \gsap-lenis-learn\src\App.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { useState, useRef, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -22,7 +30,7 @@ function App() {
 
   return (
     <>
-      <div>测试</div>
+      <div className="bg-red-300">测试</div>
       <div>
         <span ref={logoRef}>
           <img src={viteLogo} onClick={test} className="logo" alt="Vite logo" />
---
+++ gsap-lenis-learn/tailwind.config.mjs
@@ -0,0 +1,17 @@
+/*
+ * @Author: cpasion-office-win10 373704015@qq.com
+ * @Date: 2024-02-21 09:15:00
+ * @LastEditors: capsion_surfacePro7 capsion@surfacePro2.com
+ * @LastEditTime: 2025-03-05 21:37:19
+ * @FilePath: \cps-blog-docusaurus-v3\tailwind.config.js
+ * @Description: tailwind配置
+ */
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
