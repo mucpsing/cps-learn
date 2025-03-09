70d4be28fd688679019b070da4f92df7636b0b3b
Capsion <373704015@qq.com>
2025-03-09 22:27:05 +0800

    feat: add demo floder

--- gsap-lenis-learn/src/App.tsx
+++ gsap-lenis-learn/src/App.tsx
@@ -6,63 +6,66 @@
  * @FilePath: \gsap-lenis-learn\src\App.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { useState, useRef, useEffect } from "react";
-import "./App.css";
+import { useState, useRef, useEffect } from 'react';
+import './App.css';
 
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useGSAP } from "@gsap/react";
-import { ReactLenis, useLenis } from "lenis/react";
-import "lenis/dist/lenis.css";
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+import { ReactLenis, useLenis } from 'lenis/react';
+import 'lenis/dist/lenis.css';
 
-import { DEFAULT_SUB_COLOR } from "./store/config";
+import { DEFAULT_SUB_COLOR } from './store/config';
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 ScrollTrigger.defaults({
   markers: {
-    startColor: "green",
-    endColor: "red",
-    fontSize: "12px",
+    startColor: 'green',
+    endColor: 'red',
+    fontSize: '12px',
   },
 });
 
 gsap.defaults({
-  ease: "none",
+  ease: 'none',
 });
 
 function App() {
+  const lenisRef = useRef<HTMLDivElement>(null);
   const [count, setCount] = useState(0);
   const mainRef = useRef<HTMLDivElement>(null);
   const trackWarp = useRef<HTMLDivElement>(null);
 
-  useGSAP(
-    (_context, contextSafe) => {
-      gsap.to(trackWarp.current, {
-        x: () => {
-          const res = (DEFAULT_SUB_COLOR.length - 1) * window.innerWidth;
-          console.log({ res });
-          return `-${res}`;
-        },
-        scrollTrigger: {
-          trigger: trackWarp.current,
-          start: "center center",
-          end: () => "+=" + (trackWarp.current.scrollWidth - window.innerWidth),
-          scrub: 1,
-          pin: true,
-          invalidateOnRefresh: true,
-          id: "id-one",
-        },
-      });
-    },
-    { scope: mainRef }
-  );
+  // useGSAP(
+  //   (_context, _contextSafe) => {
+  //     gsap.to(trackWarp.current, {
+  //       x: () => {
+  //         const res = (DEFAULT_SUB_COLOR.length - 1) * window.innerWidth;
+  //         console.log({ res });
+  //         return `-${res}`;
+  //       },
+  //       scrollTrigger: {
+  //         trigger: trackWarp.current,
+  //         start: 'center center',
+  //         end: () => '+=' + (trackWarp.current.scrollWidth - window.innerWidth),
+  //         scrub: 1,
+  //         pin: true,
+  //         invalidateOnRefresh: true,
+  //         id: 'id-one',
+  //       },
+  //     });
+  //   },
+  //   { scope: mainRef }
+  // );
+
+  useEffect(() => {});
 
   return (
-    <ReactLenis root>
+    <ReactLenis root ref={lenisRef}>
       <main ref={mainRef} className="main h-screen relative w-screen">
         <header className="bg-red-300 w-[100vw] h-[100vh]">测试2</header>
 
-        <section ref={trackWarp} className={["track", "flex-nowrap h-full items-center justify-center inline-flex relative bg-gray-500"].join(" ")}>
+        <section ref={trackWarp} className={['track', 'flex-nowrap h-full items-center justify-center inline-flex relative bg-gray-500'].join(' ')}>
           {DEFAULT_SUB_COLOR.map((item, key) => {
             return (
               <div key={key} className="retracklative w-screen h-[80vh]">
@@ -72,10 +75,10 @@ function App() {
           })}
         </section>
 
-        <footer className={["flex flex-col items-center justify-center w-full py-10"].join(" ")}>
+        <footer className={['flex flex-col items-center justify-center w-full py-10'].join(' ')}>
           <h1>Vite + React</h1>
           <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+            <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
             <p>
               Edit <code>src/App.tsx</code> and save to test HMR
             </p>
