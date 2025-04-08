b5f737646f0aaa03b45b02a19fa3a01352941546
cpasion-office-win10 <373704015@qq.com>
2025-04-08 20:43:08 +0800

    feat: loading

--- gsap-lenis-learn/src/Test.tsx
+++ gsap-lenis-learn/src/Test.tsx
@@ -2,15 +2,16 @@
  * @Author: Capsion 373704015@qq.com
  * @Date: 2025-04-02 12:14:23
  * @LastEditors: cpasion-office-win10 373704015@qq.com
- * @LastEditTime: 2025-04-03 09:50:33
+ * @LastEditTime: 2025-04-08 09:27:41
  * @FilePath: \gsap-lenis-learn\src\Test.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { useState, useRef, useEffect } from "react";
 // import MatrixCSS from "@src/components/MatrixCSS";
 // import BlurBall from "@src/components/BlurBalls";
-// import BackgroundBubble from "@site/src/components/BackgroundBubble";
-import Loading from "@site/src/components/Loading";
+import BackgroundBubble from "@site/src/components/BackgroundBubble";
+
+import Loading from "@site/src/components/Loading/index";
 export default function App() {
   const testRef = useRef<HTMLDivElement>(null);
 
@@ -18,9 +19,5 @@ export default function App() {
     console.log("test init");
   }, []);
 
-  return (
-    <div ref={testRef} className="absolute top-0 left-0 w-full h-screen flex items-center justify-center">
-      <Loading></Loading>
-    </div>
-  );
+  return <Loading></Loading>;
 }
---
+++ gsap-lenis-learn/src/components/Loading/loading.ts
@@ -0,0 +1,22 @@
+/*
+ * @Author: cpasion-office-win10 373704015@qq.com
+ * @Date: 2025-04-07 15:53:08
+ * @LastEditors: cpasion-office-win10 373704015@qq.com
+ * @LastEditTime: 2025-04-08 09:25:52
+ * @FilePath: \gsap-lenis-learn\src\components\Loading\loading.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loadingWarpDivEl = document.createElement("div");
+  const loadingTextList: HTMLDivElement[] = [];
+
+  // 创建loading div
+  const mainText = "<Capsion' Hub/>";
+
+  const subText = "loading...";
+
+  document.body.prepend;
+
+  console.log("loading... 123");
+});
