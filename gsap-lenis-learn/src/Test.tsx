b50c5a076322500702f4a3ecb6bb331256161741
Capsion <373704015@qq.com>
2025-04-13 23:04:16 +0800

    feat: 优化北京bubble

--- gsap-lenis-learn/src/Test.tsx
+++ gsap-lenis-learn/src/Test.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: Capsion 373704015@qq.com
  * @Date: 2025-04-02 12:14:23
- * @LastEditors: cpasion-office-win10 373704015@qq.com
- * @LastEditTime: 2025-04-08 09:27:41
+ * @LastEditors: Capsion 373704015@qq.com
+ * @LastEditTime: 2025-04-12 23:58:03
  * @FilePath: \gsap-lenis-learn\src\Test.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -19,5 +19,5 @@ export default function App() {
     console.log("test init");
   }, []);
 
-  return <Loading></Loading>;
+  return <BackgroundBubble></BackgroundBubble>;
 }
--- gsap-lenis-learn/src/components/BackgroundBubble/index.tsx
+++ gsap-lenis-learn/src/components/BackgroundBubble/index.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: Capsion 373704015@qq.com
  * @Date: 2025-03-27 21:14:24
- * @LastEditors: cpasion-office-win10 373704015@qq.com
- * @LastEditTime: 2025-04-07 15:30:00
+ * @LastEditors: Capsion 373704015@qq.com
+ * @LastEditTime: 2025-04-13 21:50:02
  * @FilePath: \gsap-lenis-learn\src\components\BackgroundBubble\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,32 +12,53 @@ import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 
 interface GlowBackgroundProps {
-  glowColor: string; // 光晕颜色
-  scaleRange: [number, number]; // 光晕尺寸比例（相对于视口）
-  blur: number; // 模糊度
-  baseSize: number;
-  parallaxIntensity: number; // 视差强度
-  count: number;
+  BubbleOptions?: BubbleOptions[];
 }
 
-const DEFAULT_PROPS = { glowColor: "#FC1E4F", scaleRange: [0.8, 2], blur: 10, baseSize: 200, count: 3 };
+interface BubbleOptions {
+  blur: number; // 模糊度
+  color: string; // 光晕颜色
+  size: number; // 光晕尺寸（相对于视口）
+  x: string;
+  y: string;
+}
 
+const DEFAULT_PROPS: Required<GlowBackgroundProps> = { BubbleOptions: [{ x: "30%", y: "25%", blur: 10, color: "#FC1E4F", size: 300 }] };
+
+/**
+ * @description: 返回一个随机数，
+ * @param {number} min
+ * @param {number} max
+ * @param {number} step
+ * @return {*}
+ */
 const getRandomFloat = (min: number, max: number, step: number): number => {
   // Calculate the number of possible steps between min and max
   const steps = (max - min) / step;
 
-  // Generate a random number of steps (0 to steps inclusive)
   const randomSteps = Math.floor(Math.random() * (steps + 1));
 
-  // Calculate and return the random float
-  console.log(min + randomSteps * step);
   return min + randomSteps * step;
 };
-export default function GlowBackground(props: GlowBackgroundProps) {
-  props = { ...DEFAULT_PROPS, ...props };
+export default function GlowBackground(_props: GlowBackgroundProps = {}) {
+  const props: Required<GlowBackgroundProps> = { ...DEFAULT_PROPS, ..._props };
 
   const backgroundRef = useRef<HTMLDivElement | null>(null);
   const ballRef = useRef<Array<HTMLDivElement | null>>([]);
+
+  const { contextSafe } = useGSAP({ scope: backgroundRef });
+
+  const onMouseLeave = contextSafe((e: MouseEvent) => {
+    console.log("onMouseMove: ...");
+
+    // 小球回到原来的位置
+    gsap.to(ballRef.current, {
+      x: 0,
+      y: 0,
+      duration: 0.6,
+      ease: "power3.out",
+    });
+  });
 
   useGSAP(
     (_context, contextSafe) => {
@@ -49,9 +70,9 @@ export default function GlowBackground(props: GlowBackgroundProps) {
 
         // 移动小球
         gsap.to(ballRef.current, {
-          x: `${-x / 8}px`,
-          y: `${-y / 8}px`,
-          duration: 3,
+          x: `${-x / 10}px`,
+          y: `${-y / 10}px`,
+          duration: 0.6,
           ease: "power3.out",
         });
       });
@@ -62,37 +83,31 @@ export default function GlowBackground(props: GlowBackgroundProps) {
         backgroundRef.current?.removeEventListener("mousemove", onMouseMove);
       };
     },
-    { scope: backgroundRef }
+    { scope: backgroundRef, dependencies: [props] }
   );
 
   return (
-    <div ref={backgroundRef} className="overflow-hidden absolute top-0 left-0 w-full h-screen">
-      {Array(props.count)
-        .fill(1)
-        .map((_, index) => {
-          const size = props.baseSize * getRandomFloat(props.scaleRange[0], props.scaleRange[1], 0.1);
-          const coords = getRandomFloat(0, 100, 0.3);
-          console.log({ size });
-
-          return (
-            <div
-              key={index}
-              ref={(el) => {
-                ballRef.current[index] = el;
-              }}
-              className="-translate-x-1/2 -translate-y-1/2 absolute"
-              style={{
-                top: `${coords}%`,
-                left: `${coords}%`,
-                width: `${size}px`,
-                height: `${size}px`,
-                background: `radial-gradient(circle, ${hexToRgba(props.glowColor as string, 0.1)} 10%, transparent 60%)`,
-                filter: `blur(${blur}px)`,
-                borderRadius: "60%",
-              }}
-            />
-          );
-        })}
+    <div onMouseLeave={onMouseLeave} ref={backgroundRef} className="overflow-hidden absolute top-0 left-0 w-full h-screen">
+      {props.BubbleOptions.map((item, key) => {
+        return (
+          <div
+            key={key}
+            className="-translate-x-1/2 -translate-y-1/2 absolute"
+            ref={(el) => {
+              ballRef.current[key] = el;
+            }}
+            style={{
+              top: item.y,
+              left: item.x,
+              background: `radial-gradient(circle, ${hexToRgba(item.color as string, 0.1)} 50%, transparent 60%)`,
+              filter: `blur(${item.blur}px)`,
+              borderRadius: "100%",
+              width: `${item.size}px`,
+              height: `${item.size}px`,
+            }}
+          ></div>
+        );
+      })}
     </div>
   );
 }
