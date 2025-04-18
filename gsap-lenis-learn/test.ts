ab9e1baae9b9b57c9258851a9935307719022cd0
cpasion-office-win10 <373704015@qq.com>
2025-04-18 15:17:05 +0800

    feat: fast

--- gsap-lenis-learn/test.ts
+++ gsap-lenis-learn/test.ts
@@ -1,18 +1,11 @@
 /*
  * @Author: cpasion-office-win10 373704015@qq.com
- * @Date: 2025-04-07 15:53:08
+ * @Date: 2025-04-09 15:20:09
  * @LastEditors: cpasion-office-win10 373704015@qq.com
- * @LastEditTime: 2025-04-08 09:25:52
- * @FilePath: \gsap-lenis-learn\src\components\Loading\loading.ts
+ * @LastEditTime: 2025-04-09 15:20:20
+ * @FilePath: \gsap-lenis-learn\test.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+import fs from "fs";
 
-class loading {
-  constructor() {
-    console.log("loading init");
-  }
-
-  show() {
-    console.log("loading show");
-  }
-}
+console.log(fs);
