023aa5111e1071483da8ec5329e3c78260d6eec0
Capsion <373704015@qq.com>
2025-04-18 23:43:51 +0800

    feat: fast

--- gsap-lenis-learn/src/pages/Home/index.tsx
+++ gsap-lenis-learn/src/pages/Home/index.tsx
@@ -2,7 +2,7 @@
  * @Author: cpasion-office-win10 373704015@qq.com
  * @Date: 2025-03-13 10:14:49
  * @LastEditors: Capsion 373704015@qq.com
- * @LastEditTime: 2025-04-17 23:46:54
+ * @LastEditTime: 2025-04-18 07:57:47
  * @FilePath: \gsap-lenis-learn\src\App.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
